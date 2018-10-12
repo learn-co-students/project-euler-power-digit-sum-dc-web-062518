3d9c8c268d75ada3f2bebda652e460d66316d7c5
cemilycollins <c.emily.collins@gmail.com>
2018-10-12 11:17:40 -0400

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,11 @@
 # code your solution here
+
+def power_digit_sum(base, exp)
+  exponentiated_base = base**exp
+  num_array = exponentiated_base.to_s.split("")
+  sum = 0
+  num_array.each do |str|
+    sum += str.to_i
+  end
+  sum
+end
